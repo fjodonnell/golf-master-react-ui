0652550c5b292ea006f6b4cbb7ddd72889e5d958
fjodonnell <fjodonnell@me.com>
2026-02-14 20:30:54 -0500

    github pages stuff

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/golf-master-react-ui/',
     build: {
       outDir: 'build',
     },
